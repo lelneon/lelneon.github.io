edc0cfc2d30097ab5ff7a5cba79e66945b500460
Ziad Skafi <ziad.skafi12@gmail.com>
2025-09-08 20:12:42 +0400

    o no

---
+++ script.js
@@ -0,0 +1,14 @@
+function showSection(id) {
+  // hide all sections
+  document.querySelectorAll("section").forEach(section => {
+    section.classList.remove("active");
+  });
+
+  // show the chosen one
+  document.getElementById(id).classList.add("active");
+}
+
+// Show "about" by default
+document.addEventListener("DOMContentLoaded", () => {
+  showSection("about");
+});
