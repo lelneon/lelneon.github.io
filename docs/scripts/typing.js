5a59df7caf92a57ebd03fac04dd8ed35186d7526
z-sk <70140415+z-sk1@users.noreply.github.com>
2025-09-10 08:53:35 +0000

    tried

--- docs/scripts/typing.js
+++ docs/scripts/typing.js
@@ -1,52 +1,61 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const typingElement = document.getElementById("typing");
-    const messages = ["Welcome to my corner!", "Have a nice stay!", "Enjoy!"]
-    let msgIndex = 0;
-    let charIndex = 0;
-    let deleting = false;
-    let cursorInterval;
+    const typingElements = document.querySelectorAll(".typing");
 
-    typingElement.style.borderRight = "2px solid white";
-    function startCursorBlink() {
-        cursorInterval = setInterval(() => {
-            typingElement.style.borderRight =
-                typingElement.style.borderRight === "2px solid white"
-                    ? "2px solid transparent"
-                    : "2px solid white";
-        }, 500);
-    }
+    typingElements.forEach((typingElement) => {
+        const messages = null;
+        let msgIndex;
+        let charIndex = 0;
+        let deleting = false;
+        let cursorInterval;
 
-    function stopCursorBlink() {
-        clearInterval(cursorInterval);
         typingElement.style.borderRight = "2px solid white";
-    }
-
-    function type() {
-        const currentMsg = messages[msgIndex]
-
-        if (!deleting) {
-            typingElement.innerHTML = currentMsg.slice(0, charIndex + 1);
-            charIndex++;
-
-            if (charIndex === currentMsg.length) {
-                startCursorBlink()
-                deleting = true;
-                setTimeout(type, 2500)
-                return;
-            }
-        } else {
-            stopCursorBlink()
-            typingElement.innerHTML = currentMsg.slice(0, charIndex - 1);
-            charIndex--;
-
-            if (charIndex === 0) {
-                deleting = false;
-                msgIndex = (msgIndex + 1) % messages.length;
-            }
+        function startCursorBlink() {
+            cursorInterval = setInterval(() => {
+                typingElement.style.borderRight =
+                    typingElement.style.borderRight === "2px solid white"
+                        ? "2px solid transparent"
+                        : "2px solid white";
+            }, 500);
         }
 
-        setTimeout(type, deleting ? 50 : 100);
-    }
+        function stopCursorBlink() {
+            clearInterval(cursorInterval);
+            typingElement.style.borderRight = "2px solid white";
+        }
 
-    type();
+        function type() {
+            let currentMsg;
+            if (typeElement.id === "spec") {
+                currentMsg = ["Welcome!", "Have a nice stay!", "Enjoy!"];
+                msgIndex = 0;
+            } else {
+                currentMsg = typingElement.innerText;
+            }
+
+            if (!deleting) {
+                typingElement.innerHTML = currentMsg.slice(0, charIndex + 1);
+                charIndex++;
+
+                if (charIndex === currentMsg.length) {
+                    startCursorBlink()
+                    deleting = true;
+                    setTimeout(type, 2500)
+                    return;
+                }
+            } else {
+                stopCursorBlink()
+                typingElement.innerHTML = currentMsg.slice(0, charIndex - 1);
+                charIndex--;
+
+                if (charIndex === 0) {
+                    deleting = false;
+                    if (msgIndex) msgIndex = (msgIndex + 1) % messages.length;
+                }
+            }
+
+            setTimeout(type, deleting ? 50 : 100);
+        }
+
+        type();
+    });
 });
